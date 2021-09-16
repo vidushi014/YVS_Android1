8ce678160193ebe3465a6f43399f5339bf06a557
Vidushi Singh <76548398+vidushi014@users.noreply.github.com>
2021-09-17 01:39:07 +0530

    login and registration firebase added

--- app/src/main/java/com/example/voice_notes/MainActivity.java
+++ app/src/main/java/com/example/voice_notes/MainActivity.java
@@ -1,23 +1,81 @@
 package com.example.voice_notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText etLogEmail;
+    EditText etLogPassword;
+    ImageButton btnLogin;
+
+    private FirebaseAuth mAuth;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etLogEmail = findViewById(R.id.editTextTextEmailAddress);
+        etLogPassword = findViewById(R.id.editTextTextPassword);
+        btnLogin = findViewById(R.id.imageButton);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(view -> {
+            LoginUser();
+        });
+
     }
-    public void open(View v){
+
+    private void LoginUser() {
+        String email = etLogEmail.getText().toString();
+        String password = etLogPassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            etLogEmail.setError("Email cannot be empty");
+            etLogEmail.requestFocus();
+        } else if (TextUtils.isEmpty(password)) {
+            etLogPassword.setError("Enter password");
+            etLogPassword.requestFocus();
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "user Login successful", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(MainActivity.this, MainActivity.class));
+                    } else {
+                        Toast.makeText(MainActivity.this, "Login failed" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+
+    public void linkToRegister(View v) {
         Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
-        Intent intent =new Intent(this,RegistrationActivity.class);
+        Intent intent = new Intent(this, RegistrationActivity.class);
         startActivity(intent);
 
     }
+
+
 }
--- app/src/main/java/com/example/voice_notes/RegistrationActivity.java
+++ app/src/main/java/com/example/voice_notes/RegistrationActivity.java
@@ -1,14 +1,87 @@
 package com.example.voice_notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    EditText etRegEmail;
+    EditText etRegPassword;
+    EditText etRegConfPass;
+    ImageButton btnRegister;
+
+    FirebaseAuth mAuth;
+
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        etRegEmail = findViewById(R.id.editTextTextEmailAddress2);
+        etRegPassword = findViewById(R.id.editTextTextPassword2);
+        etRegConfPass = findViewById(R.id.editTextTextPassword3);
+        btnRegister = findViewById(R.id.imageButton2);
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegister.setOnClickListener(view ->{
+            createUser();
+        });
+    }
+    private void createUser(){
+        String email = etRegEmail.getText().toString() ;
+        String password = etRegPassword.getText().toString();
+        String confpass = etRegConfPass.getText().toString();
+
+        if(TextUtils.isEmpty(email)){
+            etRegEmail.setError("Email cannot be empty");
+            etRegEmail.requestFocus();
+        }
+
+        else if (TextUtils.isEmpty(password)){
+            etRegPassword.setError("Enter password");
+            etRegPassword.requestFocus();
+        }
+
+        else if (TextUtils.isEmpty(confpass)){
+            etRegConfPass.setError("please confirm password");
+            etRegConfPass.requestFocus();
+        }
+        else if(!confpass.equals(password)){
+            etRegConfPass.setError("password don't match");
+            etRegConfPass.requestFocus();
+        }
+        else {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(RegistrationActivity.this, "user registered successfully", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegistrationActivity.this,MainActivity.class));
+                    }
+                    else{
+                        Toast.makeText(RegistrationActivity.this, "Registration failed"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+
     }
 }
