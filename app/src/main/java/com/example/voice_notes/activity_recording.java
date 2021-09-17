6a2749323da1377121f823f5279743e530f45200
Shivanshu22-ui <shivasingh310102@gmail.com>
2021-09-18 01:03:40 +0530

    audio errors

--- app/src/main/java/com/example/voice_notes/activity_recording.java
+++ app/src/main/java/com/example/voice_notes/activity_recording.java
@@ -1,9 +1,13 @@
 package com.example.voice_notes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.media.Image;
 import android.media.MediaRecorder;
 import android.os.Bundle;
@@ -23,11 +27,26 @@ public class activity_recording extends AppCompatActivity {
     private MediaRecorder recorder;
     private String fileName=null;
     private static final String LOG_TAG="Record_log";
+    private boolean permissionToRecordAccepted = false;
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case REQUEST_RECORD_AUDIO_PERMISSION:
+                permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
+        if (!permissionToRecordAccepted ) finish();
+
+    }
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
         mRecordLable=findViewById(R.id.recordLable);
         mRecordBtn=findViewById(R.id.recordBtn);
         fileName= Environment.getExternalStorageDirectory().getAbsolutePath();
@@ -56,14 +75,14 @@ public class activity_recording extends AppCompatActivity {
         try {
             recorder.prepare();
         } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
+            Log.e(LOG_TAG, "prepare() failed"+e);
         }
 
-        recorder.start();
+//        recorder.start();
     }
 
     private void stopRecording() {
-        recorder.stop();
+//        recorder.stop();
         recorder.release();
         recorder = null;
     }
