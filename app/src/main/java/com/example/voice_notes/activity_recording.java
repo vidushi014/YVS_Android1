40951b4de187f484b8adcb7b68b9e547e2a06468
Shivanshu22-ui <shivasingh310102@gmail.com>
2021-09-18 12:39:21 +0530

    recorder and storage bug resolved

--- app/src/main/java/com/example/voice_notes/activity_recording.java
+++ app/src/main/java/com/example/voice_notes/activity_recording.java
@@ -3,9 +3,12 @@ package com.example.voice_notes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.media.Image;
@@ -19,6 +22,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.io.File;
 import java.io.IOException;
 
 public class activity_recording extends AppCompatActivity {
@@ -30,6 +34,9 @@ public class activity_recording extends AppCompatActivity {
     private boolean permissionToRecordAccepted = false;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+
+    private static final String TAG = "Activity record";
+//    REQUESTING PERMISSIONS FOR AUDIO AND STORAGE
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -41,16 +48,23 @@ public class activity_recording extends AppCompatActivity {
         if (!permissionToRecordAccepted ) finish();
 
     }
+
+
+    // MAIN FUNCTION
+
     @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recording);
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+//        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
         mRecordLable=findViewById(R.id.recordLable);
         mRecordBtn=findViewById(R.id.recordBtn);
-        fileName= Environment.getExternalStorageDirectory().getAbsolutePath();
-        fileName+="/recorded_audio.3gp";
+//        fileName= Environment.getExternalStorageDirectory().getAbsolutePath();
+//        fileName+="/recorded_audio1.3gp";
+        if (isMicrophonePresent()){
+            getMicrophonePermission();
+        }
         mRecordBtn.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -65,25 +79,50 @@ public class activity_recording extends AppCompatActivity {
             }
         });
     }
+    // START RECORDING CODE
+
     private void startRecording() {
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setOutputFile(fileName);
+        recorder.setOutputFile(getRecordingFilePath());
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
         try {
             recorder.prepare();
+            recorder.start();
         } catch (IOException e) {
             Log.e(LOG_TAG, "prepare() failed"+e);
         }
 
-//        recorder.start();
-    }
 
+    }
+// STOP RECORDING CODE
     private void stopRecording() {
-//        recorder.stop();
+        recorder.stop();
         recorder.release();
         recorder = null;
+    }
+    // PRESENCE OF MICROPHONE
+    private boolean isMicrophonePresent(){
+        if(this.getPackageManager().hasSystemFeature(PackageManager.FEATURE_MICROPHONE)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    // Permission for microphone
+    private void getMicrophonePermission(){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO)==PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.RECORD_AUDIO},REQUEST_RECORD_AUDIO_PERMISSION);
+        }
+    }
+    // Storing the file path
+    private String getRecordingFilePath(){
+        ContextWrapper contextWrapper=new ContextWrapper(getApplicationContext());
+        File musicDirectory =contextWrapper.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
+        File file=new File(musicDirectory,"record.mp3");
+        Log.i(TAG, "getRecordingFilePath: "+file.getPath());
+        return file.getPath();
     }
 }
